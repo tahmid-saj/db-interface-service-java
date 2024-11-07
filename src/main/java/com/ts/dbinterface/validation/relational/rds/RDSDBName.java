3a526a693278bda243e6a7f61bb747d463a4abab
tahmid-saj <tahmid.saj@gmail.com>
2024-11-06 20:37:02 -0500

    rds ops samples

--- src/main/java/com/ts/dbinterface/validation/relational/rds/RDSDBName.java
+++ src/main/java/com/ts/dbinterface/validation/relational/rds/RDSDBName.java
@@ -1,16 +1,16 @@
 package com.ts.dbinterface.validation.relational.rds;
 
-import com.ts.dbinterface.validation.relational.rds.validator.RDSTableValidator;
+import com.ts.dbinterface.validation.relational.rds.validator.RDSDBValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Constraint(validatedBy = RDSTableValidator.class)
+@Constraint(validatedBy = RDSDBValidator.class)
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface RDSTableName {
+public @interface RDSDBName {
     String message() default "Invalid field value. Only 0-9, a-z, A-Z characters are allowed.";
 
     Class<?>[] groups() default {};
